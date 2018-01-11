88562f626f125906455bfd12e0feb13ef1825bcf
Ido Shoshani <idoshoshani@gmail.com>
2018-01-11 19:31:45 +0200

    env BuildNumber

--- src/main/java/com/experitest/auto/BaseTest.java
+++ src/main/java/com/experitest/auto/BaseTest.java
@@ -19,6 +19,7 @@ public class BaseTest {
 		dc.setCapability("reportDirectory", "reports");
 		dc.setCapability("reportFormat", "xml");
 		dc.setCapability("stream","IdoSJenkinsTask");
+		dc.setCapability("build.number", System.getenv("BUILD.NUMBER"));
 		dc.setCapability("accessKey",getProperty("accessKey", cloudProperties));
 
 	}
